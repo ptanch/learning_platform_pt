0e70ab4b9d4f340c045bba2e261a38ab93c531f1
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-13 22:43:53 +0300

    добавлены настройки кастомизации модели user

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 import os
 from pathlib import Path
 
+from django.conf.global_settings import AUTH_USER_MODEL
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -100,3 +101,5 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "users.User"
