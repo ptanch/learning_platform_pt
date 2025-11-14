02d308e8f610b4d501e2a01ea215cf57f12310b0
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-14 15:13:23 +0300

    созданы и описаны пути для отображения crud операций модели lesson

--- lms/urls.py
+++ lms/urls.py
@@ -1,6 +1,14 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from lms.views import CourseViewSet
+from lms.views import (
+    LessonCreateApiView,
+    LessonListApiView,
+    LessonUpdateApiView,
+    LessonRetrieveApiView,
+    LessonDestroyApiView
+)
 from lms.apps import LmsConfig
 
 app_name = LmsConfig.name
@@ -9,6 +17,12 @@ router = SimpleRouter()
 router.register("", CourseViewSet)
 
 
-urlpatterns = []
+urlpatterns = [
+    path("lesson/", LessonListApiView.as_view(), name="lesson_list"),
+    path("lesson/<int:pk>/", LessonRetrieveApiView.as_view(), name="lesson_retrieve"),
+    path("lesson/create/", LessonCreateApiView.as_view(), name="lesson_create"),
+    path("lesson/<int:pk>/delete/", LessonDestroyApiView.as_view(), name="lesson_delete"),
+    path("lesson/<int:pk>/update/", LessonUpdateApiView.as_view(), name="lesson_update"),
+]
 
 urlpatterns += router.urls
