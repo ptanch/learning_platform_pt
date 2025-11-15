4b9196e7915a4e39c746f0cdeeb2e9d445d5714f
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-15 23:56:44 +0300

    созданы контроллеры для приложения users

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveUpdateAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateApiView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserUpdateApiView(UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserProfileApiView(RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
