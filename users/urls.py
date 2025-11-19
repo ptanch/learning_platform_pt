957d4d3918368ba1eef5a3b4263e1d5a25093477
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-19 16:24:48 +0300

    добавлен путь для list.api.view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from users import views
 from users.apps import UsersConfig
+from users.views import PaymentsListApiView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('create/', views.UserCreateApiView.as_view(), name='create'),
     path('profile/', views.UserProfileApiView.as_view(), name='profile'),
+
+    path("payments/", PaymentsListApiView.as_view(), name="payments_list"),
 ]
