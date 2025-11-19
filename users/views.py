bf1f2c8dde5026d183e9321efa8fbbb2a14b5176
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-19 16:10:08 +0300

    создан list.api.view для payments, поддерживающий сортировку и фильтрацию, проверено форматером black

--- users/views.py
+++ users/views.py
@@ -1,8 +1,14 @@
-from rest_framework.generics import CreateAPIView, UpdateAPIView, RetrieveUpdateAPIView
+from rest_framework.filters import OrderingFilter
+from rest_framework.generics import (
+    CreateAPIView,
+    UpdateAPIView,
+    RetrieveUpdateAPIView,
+    ListAPIView,
+)
 from rest_framework.permissions import IsAuthenticated
 
-from users.models import User
-from users.serializers import UserSerializer
+from users.models import User, Payments
+from users.serializers import UserSerializer, PaymentsSerializer
 
 
 class UserCreateApiView(CreateAPIView):
@@ -21,3 +27,31 @@ class UserProfileApiView(RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+class PaymentsListApiView(ListAPIView):
+    queryset = Payments.objects.all()
+    serializer_class = PaymentsSerializer
+
+    filter_backends = [OrderingFilter]
+    ordering_fields = ["payment_date"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+
+        # Фильтрация по курсу
+        paid_course = self.request.query_params.get("paid_course")
+        if paid_course:
+            queryset = queryset.filter(paid_course_id=paid_course)
+
+        # Фильтрация по уроку
+        paid_lesson = self.request.query_params.get("paid_lesson")
+        if paid_lesson:
+            queryset = queryset.filter(paid_lesson_id=paid_lesson)
+
+        # Фильтрация по способу оплаты
+        payment_method = self.request.query_params.get("payment_method")
+        if payment_method:
+            queryset = queryset.filter(payment_method=payment_method)
+
+        return queryset
