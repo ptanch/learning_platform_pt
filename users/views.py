bdf387f200c3d5e692ab244930c4111ed7fef62a
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-20 23:41:17 +0300

    добавлен метод проверки того, активен ли пользователь

--- users/views.py
+++ users/views.py
@@ -15,6 +15,11 @@ class UserCreateApiView(CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    def perform_create(self, serializer):
+        user = serializer.save(is_active=True)
+        user.set_password(user.password)
+        user.save
+
 
 class UserUpdateApiView(UpdateAPIView):
     queryset = User.objects.all()
