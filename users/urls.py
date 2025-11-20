1185f96455c118ecc9a1841b69ffe2debb17a138
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-20 23:41:49 +0300

    добавлен путь для регистрации пользователя

--- users/urls.py
+++ users/urls.py
@@ -13,6 +13,7 @@ urlpatterns = [
     path("create/", views.UserCreateApiView.as_view(), name="create"),
     path("profile/", views.UserProfileApiView.as_view(), name="profile"),
     path("payments/", PaymentsListApiView.as_view(), name="payments_list"),
+    path("register/", UserCreateApiView.as_view(), name="register"),
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
