436820f42b1ff4155664fcc97dbe1f31e9220757
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-20 23:39:48 +0300

    добавлен функционал: токены

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 from users import views
 from users.apps import UsersConfig
-from users.views import PaymentsListApiView
+from users.views import PaymentsListApiView, UserCreateApiView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path('create/', views.UserCreateApiView.as_view(), name='create'),
-    path('profile/', views.UserProfileApiView.as_view(), name='profile'),
-
+    path("create/", views.UserCreateApiView.as_view(), name="create"),
+    path("profile/", views.UserProfileApiView.as_view(), name="profile"),
     path("payments/", PaymentsListApiView.as_view(), name="payments_list"),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
