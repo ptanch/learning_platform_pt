da3c250df3364f3a11cb8689c70e5ebe5bba4458
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-21 15:11:02 +0300

    скоректирован метод perform_create, для корректного сохранения пароля

--- users/views.py
+++ users/views.py
@@ -18,7 +18,7 @@ class UserCreateApiView(CreateAPIView):
     def perform_create(self, serializer):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
-        user.save
+        user.save()
 
 
 class UserUpdateApiView(UpdateAPIView):
