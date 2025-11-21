710c4c42ee3a906b6c780f8724b80215a924ea05
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-21 23:26:08 +0300

    добавлен класс is.moder для решулирования группы модераторов и их прав доступа

---
+++ users/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import permissions
+
+
+class IsModer(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        return request.user.groups.filter(name="moders").exists()
