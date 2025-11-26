019303f71214600efdf76915a9fb36d95f45e04a
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-26 17:25:44 +0300

    добавлена ифнормация в сериализатор, чтобы отображать признак подписки текущего пользователя на курс

--- lms/serializers.py
+++ lms/serializers.py
@@ -1,14 +1,29 @@
 from rest_framework.fields import SerializerMethodField
 from rest_framework import serializers
 
-from lms.models import Course, Lesson
+from lms.models import Course, Lesson, Subscription
 from lms.validators import validate_video_url
 
 
 class CourseSerializer(serializers.ModelSerializer):
+    is_subscribed = serializers.SerializerMethodField()
+
     class Meta:
         model = Course
         fields = "__all__"
+
+    def get_is_subscribed(self, obj):
+        request = self.context.get("request")
+
+        if not request or request.user.is_anonymous:
+            return False
+
+        user = request.user
+
+        return Subscription.objects.filter(
+            user=user,
+            course=obj
+        ).exists()
 
 
 class LessonSerializer(serializers.ModelSerializer):
