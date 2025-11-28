67ef847506b4c09d39466b94340199032f038e9a
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-11-28 16:23:18 +0300

    добавлена маршрутизация для работы drf-spectacular

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,12 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
@@ -31,4 +37,14 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ]
