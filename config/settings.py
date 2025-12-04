b4da387d259fa637e0156689b03ee35b660e2638
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-04 17:00:04 +0300

    добавлены настройки почтового сервера для django

--- config/settings.py
+++ config/settings.py
@@ -159,3 +159,10 @@ if CACHE_ENABLED:
             'LOCATION': os.getenv('LOCATION'),
         }
     }
+
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 465))
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "False") == "True"
+EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", "True") == "True"
