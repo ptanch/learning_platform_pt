c1de8e776803a9aa75d1aff438562553395adfa0
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-04 19:53:16 +0300

    прописан докстринг для paginations.py

--- lms/paginations.py
+++ lms/paginations.py
@@ -2,6 +2,12 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class CustomPagination(PageNumberPagination):
+    """
+    Настраивает параметры разбиения данных на страницы для API-ответов:
+    - фиксированный размер страницы по умолчанию;
+    - возможность динамического изменения размера страницы через query-параметр;
+    - ограничение максимального размера страницы.
+    """
     page_size = 5
     page_size_query_param = "page_size"
     max_page_size = 10
