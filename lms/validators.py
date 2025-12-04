121598671f95dc511d9083c3fad874f53145e51b
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-04 20:01:10 +0300

    прописаны докстринги для lms/validators.py

--- lms/validators.py
+++ lms/validators.py
@@ -2,6 +2,7 @@ from rest_framework.serializers import ValidationError
 
 
 def validate_video_url(value):
+    """Валидация ссылок: принимаются только от youtube"""
     if not value.startswith("https://www.youtube.com"):
         raise ValidationError("Использованы сторонние ссылки")
     return value
