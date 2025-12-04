9977b7fe39ea493f4b1541bc9a2567244c9e171c
Tatiana Pitieva <tatyane.liss@gmail.com>
2025-12-04 20:28:56 +0300

    прописаны докстринги для users/views.py

--- users/views.py
+++ users/views.py
@@ -1,4 +1,3 @@
-from requests import session
 from rest_framework.filters import OrderingFilter
 from rest_framework.generics import (
     CreateAPIView,
@@ -15,36 +14,59 @@ from users.services import create_stripe_price, create_stripe_session, create_st
 
 
 class UserCreateApiView(CreateAPIView):
+    """
+    API view для создания пользователя.
+    Позволяет регистрировать новых пользователей без аутентификации.
+    При создании автоматически активирует аккаунт (is_active=True) и корректно хеширует пароль
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = (AllowAny,)
 
     def perform_create(self, serializer):
+        """Создаёт пользователя, активирует его и устанавливает хешированный пароль"""
         user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
 
 
 class UserUpdateApiView(UpdateAPIView):
+    """
+    API view для обновления данных пользователя.
+    Требует аутентификации (наследуется от базового класса).
+    Позволяет изменять любые поля пользователя через UserSerializer
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserProfileApiView(RetrieveUpdateAPIView):
+    """
+    API view для получения и обновления профиля текущего пользователя.
+    Доступ разрешён только аутентифицированным пользователям.
+    Всегда работает с объектом запроса пользователя (request.user)
+    """
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
     def get_object(self):
+        """Возвращает объект текущего пользователя из запроса"""
         return self.request.user
 
 
 class UserDeleteApiView(DestroyAPIView):
+    """
+    API view для удаления пользователя.
+    Доступ разрешён только аутентифицированным пользователям.
+    Удаляет указанный объект пользователя из базы данных
+    """
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
 
 class PaymentsListApiView(ListAPIView):
+    """API view для получения списка платежей с фильтрацией и сортировкой"""
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
 
@@ -52,6 +74,7 @@ class PaymentsListApiView(ListAPIView):
     ordering_fields = ["payment_date"]
 
     def get_queryset(self):
+        """Формирует отфильтрованный набор платежей на основе параметров запроса"""
         queryset = super().get_queryset()
 
         # Фильтрация по курсу
@@ -73,10 +96,12 @@ class PaymentsListApiView(ListAPIView):
 
 
 class PaymentsCreateApiView(CreateAPIView):
+    """API view для создания платежа и интеграции со Stripe"""
     queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
 
     def perform_create(self, serializer):
+        """Создаёт платёж и настраивает интеграцию со Stripe"""
         payment = serializer.save(user=self.request.user)
 
         course = payment.paid_course
